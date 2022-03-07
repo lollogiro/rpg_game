4c812150bec7edecfecb73b1c4fc97e48e665bb6
lollogiro <lollogiro2@gmail.com>
2022-03-07 10:16:30 +0100

    setting main per test

---
+++ Player.cpp
@@ -0,0 +1,52 @@
+//
+// Created by lollo on 07/03/2022.
+//
+
+#include <curses.h>
+
+class Player {
+protected:
+    char mapSymbol;
+    int posX;
+    int posY;
+    int lifePoints;
+    WINDOW *win;
+
+public:
+    Player(char mapSymbol, int posX, int posY, int lifePoints, WINDOW *win) {
+        this->mapSymbol = mapSymbol;
+        this->posX = posX;
+        this->posY = posY;
+        this->lifePoints = lifePoints;
+        this->win = win;
+    }
+
+    Player(char mapSymbol, WINDOW *win) {
+        this->mapSymbol = mapSymbol;
+        this->posX = 1;
+        this->posY = getmaxy(win)-2;
+        this->lifePoints = 20;
+        this->win = win;
+    }
+
+    void printEntity(){
+        mvwaddch(win, posY, posX, mapSymbol);
+        wrefresh(win);
+    }
+
+    char getMapSymbol() const {
+        return mapSymbol;
+    }
+
+    int getPosX() const {
+        return posX;
+    }
+
+    int getPosY() const {
+        return posY;
+    }
+
+    int getLifePoints() const {
+        return lifePoints;
+    }
+};
--- main.cpp
+++ main.cpp
@@ -4,17 +4,30 @@
 
 #include <curses.h>
 
+#include "Player.cpp"
+
 int main(int argc, char** argv){
     initscr();
     //noecho();
     raw();
     halfdelay(2);//0.2 sec
     curs_set(0);
-    char userInput = 'a'; //random assignation
 
+    WINDOW* win = newwin(30, 60, 5, 10);
+    refresh();
+    box(win, 0, 0);
+    wrefresh(win);
+    wborder(win, '|', '|', '-', '-', '+', '+', '+', '+');
+    refresh();
+
+    Player player = Player('@', win);
+    player.printEntity();
+
+    char userInput = 'a'; //random assignation
     while(userInput != 'q'){
         userInput = getch();
         refresh();
     }
+
     endwin();
 }

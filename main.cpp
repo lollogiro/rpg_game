4b7496e075fa45dd4808084dc852b23d61e820aa
lollogiro <lollogiro2@gmail.com>
2022-03-08 11:52:28 +0100

    costruttori e metodi per il display

--- main.cpp
+++ main.cpp
@@ -22,8 +22,6 @@ int main(int argc, char** argv){
     wborder(win, '|', '|', '-', '-', '+', '+', '+', '+');
     refresh();
 
-    //prova commit
-
     Player* player = new Player('@', win);
 
     player->printPlayer();

d0c7a3b7a96e358d77668489977559f01d01c927
carolina2910 <carolinabonafe02@gmail.com>
2022-03-15 17:49:46 +0100

    prova

---
+++ Artifact.h
@@ -0,0 +1,14 @@
+//
+// Created by carol on 15/03/2022.
+//
+
+#ifndef PROGETTO_PROGRAMMAZIONE_1_ARTIFACT_H
+#define PROGETTO_PROGRAMMAZIONE_1_ARTIFACT_H
+
+
+class Artifact {
+
+};
+
+
+#endif //PROGETTO_PROGRAMMAZIONE_1_ARTIFACT_H

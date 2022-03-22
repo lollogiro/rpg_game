18920f73392edc1027144aba0b6608a31a12f185
StarboyDaviLK <rafidfardeenhoque@gmail.com>
2022-03-22 17:22:55 +0100

    prova

--- Enemy.h
+++ Enemy.h
@@ -5,6 +5,7 @@
 #ifndef PROGETTO_PROGRAMMAZIONE_ENEMY_H
 #define PROGETTO_PROGRAMMAZIONE_ENEMY_H
 
+#include <ncurses.h>
 
 class Enemy {
 
---
+++ main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by rafid on 22/03/2022.
+//
+
+#include <ncurses.h>
+#include "Enemy.h"
+
+int main(){
+    printf("Ciao\n");
+    return 0;
+}

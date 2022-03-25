b19eb8164ae80aa96f1603646d8afe913d11868e
StarboyDaviLK <rafidfardeenhoque@gmail.com>
2022-03-25 16:42:02 +0100

    prova

--- Enemy.h
+++ Enemy.h
@@ -7,9 +7,57 @@
 
 #include <ncurses.h>
 
+using namespace std;
 class Enemy {
+protected:
+    char mapChar;
+    int posX;
+    int posY;
+    int lifePoints;
+    WINDOW *win;
+
+public:
+    Enemy(char mapChar, int EposX, int EposY, int ElifePoints, WINDOW *win){
+
+    }
+
+    void printPlayer(){
+        mvwaddch(win, posY, posX, mapChar);
+        wrefresh(win);
+    }
+
+    void movePlayerUp(){
+        mvwaddch(win, posY, posX, ' ');
+        posY--;
+        if(posY < 1) posY = 1;
+        wrefresh(win);
+    }
+
+    void movePlayerDown(){
+        mvwaddch(win, posY, posX, ' ');
+        posY++;
+        if(posY > (getmaxy(win) - 2)) posY = getmaxy(win) - 2;
+        wrefresh(win);
+    }
+
+    void movePlayerLeft(){
+        mvwaddch(win, posY, posX, ' ');
+        posX--;
+        if(posX < 1) posX = 1;
+        wrefresh(win);
+    }
+
+    void movePlayerRight(){
+        mvwaddch(win, posY, posX, ' ');
+        posX++;
+        if(posX > (getmaxx(win) - 2)) posX = getmaxx(win) - 2;
+        wrefresh(win);
+    }
+
+
 
 };
+
 
 
 #endif //PROGETTO_PROGRAMMAZIONE_ENEMY_H
--- main.cpp
+++ main.cpp
@@ -2,10 +2,41 @@
 // Created by rafid on 22/03/2022.
 //
 
-#include <ncurses.h>
+#include <curses.h>
 #include "Enemy.h"
+struct Player{
+    char playersymbol;
+    WINDOW* win;
+    int posy;
+    int posx;
+};
+int main(int argc, char** argv){
+    initscr();
+    noecho();
+    raw();
+    halfdelay(2);//0.5 sec
+    curs_set(0);
 
-int main(){
-    printf("Ciao\n");
-    return 0;
+    WINDOW* win = newwin(30, 60, 5, 10);
+    keypad(win, true);
+    refresh();
+    box(win, 0, 0);
+    wrefresh(win);
+    wborder(win, '|', '|', '-', '-', '+', '+', '+', '+');
+    refresh();
+
+    Player test = { '@',win,15,30 };
+
+
+    mvwaddch(test.win, test.posy, test.posx, test.playersymbol);
+
+    int userInput = 'a';
+
+    while(userInput != 'q'){
+
+    userInput = wgetch(win);
+
+    }
+
+    endwin();
 }
